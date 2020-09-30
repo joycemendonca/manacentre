c31da3cf62b79d237a5636d9da3540e6c15cb560
Joyce Mendonça <joycemendonca2307@gmail.com>
2020-09-30 14:55:54 -0300

    edit subscribe button

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function onHeaderLoaded() {
+    $('.subscribe-button').on('click', function(){
+        $('.pop-up-subscribe').css('display','flex')
+    })
+}
+// function onHeaderLoaded(){
+    $('.close-button').on('click', function(){
+        $('.pop-up-subscribe').css('display', 'block')
+    })
+// }
+    
